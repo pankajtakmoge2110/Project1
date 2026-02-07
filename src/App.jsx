42ef70c613487f318b08915f143a8fdb83835f57
Pankaj <pankajtakmoge2110@gmail.com>
2026-02-07 17:44:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Herosection from './components/hero';
+
+function App() {
+  return (
+   <div>
+    <Navbar/>  
+    <Herosection/>
+   </div>
+  ) 
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+    <nav className="container">
+       <div className="logo">
+         <img src="/public/images/brand_logo.png" alt="Logo"></img>
+       </div>
+     <ul className="nav-btn">
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+     </ul>
+
+     <button className="login-btn">Login</button>
+    </nav>
+ );
+};
+
+export default Navbar;
